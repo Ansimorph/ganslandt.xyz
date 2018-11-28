e81a36c04ab82228f3d7aaf5f09ccfde8c1d3e38
Björn Ganslandt <b.ganslandt@gmail.com>
2018-11-28 14:20:47 +0100

    Move config to the conf files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const webpack = require("webpack");
 
 module.exports = merge.strategy({
     "module.rules": "prepend",
@@ -19,5 +20,8 @@ module.exports = merge.strategy({
         hot: true,
         open: true,
         watchContentBase: true,
+        host: "0.0.0.0",
+        inline: true,
     },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
 });
