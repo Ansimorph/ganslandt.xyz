cab0ed67ed6bd11c98e2ea1c2ecc46dc04735f2f
Björn Ganslandt <b.ganslandt@gmail.com>
2018-11-29 09:57:46 +0100

    Improve image for hidpi

--- src/helpers/srcSet.js
+++ src/helpers/srcSet.js
@@ -1,6 +1,6 @@
 const Handlebars = require("handlebars");
 
 module.exports = function(src) {
-    const image = require(`../assets/images/${src}?min=320,max=681`);
+    const image = require(`../assets/images/${src}?min=320,max=1200,steps=6`);
     return new Handlebars.SafeString(`src="${image}" srcset="${image.srcSet}"`);
 };
