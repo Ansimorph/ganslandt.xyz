3e98985a40964721ab811f5d97750eb4fac9f702
Björn Ganslandt <b.ganslandt@gmail.com>
2018-12-01 14:12:04 +0100

    Prevent reflow

--- src/helpers/srcSet.js
+++ src/helpers/srcSet.js
@@ -2,7 +2,5 @@ const Handlebars = require("handlebars");
 
 module.exports = function(src) {
     const image = require(`../assets/images/${src}?min=320,max=1200,steps=6,placeholder=true`);
-    return new Handlebars.SafeString(
-        `src="${image.placeholder}" srcset="${image.srcSet}"`,
-    );
+    return new Handlebars.SafeString(`src="${image}" srcset="${image.srcSet}"`);
 };
