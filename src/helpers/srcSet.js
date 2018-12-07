5b2319ccc8d4dee28f5c5bfe3bbf15731723240d
Björn Ganslandt <b.ganslandt@gmail.com>
2018-12-07 08:57:00 +0100

    Only process images that are meant tp be responsive

--- src/helpers/srcSet.js
+++ src/helpers/srcSet.js
@@ -1,6 +1,6 @@
 const Handlebars = require("handlebars");
 
 module.exports = function(src) {
-    const image = require(`../assets/images/${src}?min=320,max=1200,steps=6,placeholder=true`);
+    const image = require(`../assets/images/responsive/${src}?min=320,max=1200,steps=6,placeholder=true`);
     return new Handlebars.SafeString(`src="${image}" srcset="${image.srcSet}"`);
 };
