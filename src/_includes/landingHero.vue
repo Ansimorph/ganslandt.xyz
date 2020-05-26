b56977f15a7a00051b1245912b66e3a33dcae713
Björn Ganslandt <b.ganslandt@gmail.com>
2020-05-26 09:50:23 +0200

    Use sqip

--- src/_includes/landingHero.vue
+++ src/_includes/landingHero.vue
@@ -69,6 +69,8 @@ export default {
     max-width: 130base;
     height: calc(100vw * var(--hero-aspect-ratio));
     max-height: calc(130base * var(--hero-aspect-ratio));
+    background-image: url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20540%20448%22%3E%3Cfilter%20id%3D%22prefix__a%22%3E%3CfeGaussianBlur%20stdDeviation%3D%2212%22%2F%3E%3C%2Ffilter%3E%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22%23343434%22%2F%3E%3Cg%20fill-opacity%3D%22.5%22%20filter%3D%22url%28%23prefix__a%29%22%3E%3Ccircle%20r%3D%221%22%20fill%3D%22%23d6dad9%22%20transform%3D%22rotate%28103.5%2034.7%20204.2%29%20scale%28157.44923%2076.77429%29%22%2F%3E%3Cpath%20fill%3D%22%23b6b9b7%22%20d%3D%22M142.4%20197.2h232v54.9h-232z%22%2F%3E%3Ccircle%20r%3D%221%22%20fill%3D%22%23f1f2f1%22%20transform%3D%22rotate%2817%20-298.4%20980.5%29%20scale%2831.33039%2070.02642%29%22%2F%3E%3Ccircle%20r%3D%221%22%20fill%3D%22%23171a1b%22%20transform%3D%22matrix%28-22.5917%2057.23128%20-24.38782%20-9.62694%20201.2%20141%29%22%2F%3E%3Ccircle%20r%3D%221%22%20fill%3D%22%236e7170%22%20transform%3D%22matrix%2835.32537%20-32.79714%2073.66681%2079.34556%20209.7%20311%29%22%2F%3E%3Cpath%20fill%3D%22%231c1f21%22%20d%3D%22M284.6%20166.6l15.4%201.1%2013.2-84.6%2078%20150.2z%22%2F%3E%3Cpath%20fill%3D%22%233e4244%22%20d%3D%22M124.7%20311.6l64.7%2087.3%20114.6%209-85.3-83.5z%22%2F%3E%3Cpath%20fill%3D%22%23acadac%22%20d%3D%22M247.9%20296.4l-116-42.2%20225.6-57z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E%0A");
+    background-size: contain;
 
     @mixin breakpoint-m {
         width: 60%;
