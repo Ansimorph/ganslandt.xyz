e4be6baf079fb7037756044dc9ea36d0408096f5
Björn Ganslandt <b.ganslandt@gmail.com>
2024-12-30 21:12:14 +0100

    Use webc

--- .eleventy.js
+++ .eleventy.js
@@ -1,41 +1,68 @@
-const eleventyVue = require("@11ty/eleventy-plugin-vue");
-const svgContents = require("eleventy-plugin-svg-contents");
-const imageShortcode = require("./src/utils/image.js");
-const htmlnano = require("htmlnano");
+import svgContents from "eleventy-plugin-svg-contents";
+import htmlnano from "htmlnano";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import postcssMixins from "postcss-mixins";
+import postcssCustomUnit from "postcss-custom-unit";
+import pluginWebc from "@11ty/eleventy-plugin-webc";
+import postcss from "postcss";
 
-module.exports = function (config) {
-    config.addPlugin(eleventyVue, {
-        rollupPluginVueOptions: {
-            style: {
-                postcssPlugins: [
-                    require("postcss-import"),
-                    require("postcss-mixins"),
-                    require("postcss-custom-units"),
-                    require("postcss-round-float"),
-                ],
-            },
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginWebc, {
+    bundlePluginOptions: {
+      transforms: [
+        async function (content) {
+          let { type, page } = this;
+          let result = await postcss([
+            postcssMixins,
+            postcssCustomUnit({
+              units: [
+                {
+                  from: "base",
+                  convert: (val) => Math.round(val * 4) / 10 + "rem",
+                },
+              ],
+            }),
+          ]).process(content, { from: page.inputPath, to: null });
+
+          return result.css;
         },
-    });
+      ],
+    },
+  });
 
-    config.addPlugin(svgContents);
+  eleventyConfig.addPlugin(svgContents);
 
-    config.addTransform("htmlnano", async (content, outputPath) => {
-        if (!outputPath.endsWith("html")) return content;
+  eleventyConfig.addTransform("htmlnano", async (content, outputPath) => {
+    if (!outputPath.endsWith("html")) return content;
 
-        const result = await htmlnano.process(content);
-        return result.html;
-    });
+    const result = await htmlnano.process(content);
+    return result.html;
+  });
 
-    config.addPassthroughCopy({ "./src/assets/": "/" });
+  eleventyConfig.addPassthroughCopy({ "./src/assets/": "/" });
 
-    config.addJavaScriptFunction("image", imageShortcode);
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    extensions: "html",
+    formats: ["avif", "webp", "jpeg"],
+    outputDir: "./dist/images/",
+    urlPath: "/images/",
+    sharpWebpOptions: {
+      quality: 75,
+    },
+    sharpAvifOptions: {
+      quality: 40,
+      speed: 3,
+    },
+    defaultAttributes: {
+      decoding: "async",
+    },
+  });
 
-    return {
-        dir: {
-            input: "src/",
-            data: "_data",
-            includes: "_includes",
-            output: "dist",
-        },
-    };
-};
+  return {
+    dir: {
+      input: "src/",
+      data: "_data",
+      output: "dist",
+    },
+  };
+}
--- src/_data/interestSentence.js
+++ src/_data/interestSentence.js
@@ -1,2 +1,1 @@
-module.exports =
-    "Björn Ganslandt is a frontend developer who builds fast, beautiful and sustainable websites.";
+export default "Björn Ganslandt is a frontend developer who builds fast, beautiful and sustainable websites.";
