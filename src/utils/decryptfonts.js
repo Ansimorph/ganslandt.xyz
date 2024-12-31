1bc92ddcbc88ff46c3968f6132260202df49cf5b
Björn Ganslandt <b.ganslandt@gmail.com>
2024-12-31 14:46:07 +0100

    Prettier all the things

--- src/utils/decryptfonts.js
+++ src/utils/decryptfonts.js
@@ -7,14 +7,14 @@ const ENCRYPT_FILE_EXTENSION = "enc";
 const password = process.env.PASSWORD;
 
 glob(`dist/**/*.${ENCRYPT_FILE_EXTENSION}`, {}, (er, files) => {
-    files.forEach((file) => {
-        const instance = new Cryptify(file, password);
-        instance.decrypt().then((decryptedFile) => {
-            fs.rename(
-                file,
-                file.replace("." + ENCRYPT_FILE_EXTENSION, ""),
-                (err) => {},
-            );
-        });
+  files.forEach((file) => {
+    const instance = new Cryptify(file, password);
+    instance.decrypt().then((decryptedFile) => {
+      fs.rename(
+        file,
+        file.replace("." + ENCRYPT_FILE_EXTENSION, ""),
+        (err) => {},
+      );
     });
+  });
 });
